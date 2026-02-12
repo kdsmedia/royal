afa7f4104c395cc09a9b6c25bf2fa56aaf88aaca
SIDHANIE <137620314+kdsmedia@users.noreply.github.com>
2026-02-13 02:19:52 +0700

    Add files via upload

---
+++ src/articles/base/article_resources.js
@@ -0,0 +1,23 @@
+//
+// This file manages the loading of resources for article pages.
+//
+
+function ArticleResourceLoader(staged_resources) {
+    ResourceLoader.call(this, staged_resources);
+    this.__class_name__ = "ArticleResourceLoader";
+}
+setSuperClass(ArticleResourceLoader, ResourceLoader);
+
+ArticleResourceLoader.prototype.getEffectiveScreenSize = function() {
+    // We use the article content width, and scale it compared to its
+    // expected size at a 720p resolution to find our target resolution.
+    // Its messy, but should be more consistent than the window size.
+    const expectedContentWidth = 900,
+          currentContentWidth = document.getElementById("content").clientWidth,
+          rotate90ContentWidth = min(document.documentElement.clientHeight, expectedContentWidth),
+          maxPossibleContentWidth = max(rotate90ContentWidth, currentContentWidth),
+          contentWidth = maxPossibleContentWidth * window.devicePixelRatio,
+          effectiveDevicePixelRatio = contentWidth / expectedContentWidth,
+          multiplier = 1 + 2 * (effectiveDevicePixelRatio - 1);
+    return vec(1280 * multiplier, 720 * multiplier);
+};
